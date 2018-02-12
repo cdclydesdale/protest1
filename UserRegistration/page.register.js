9c7579a9d8fc05a1e6fbc48949cadc4e29a68db0
Clyde Dsouza <cdclydesdale@gmail.com>
2018-02-12 17:02:08 -0500

    User Registration Function

--- UserRegistration/page.register.js
+++ UserRegistration/page.register.js
@@ -5,6 +5,14 @@ var pageReg = function(){
   this.password = element(by.id('password'));
   this.passRepeat = element(by.id('password-repeat'));
   this.regButton = element(by.id('register_button'));
-}
 
+
+  //Function to perform User Registration
+  this.userRegister = function(){
+  this.firstName.sendKeys('First');
+  this.email.sendKeys('test@email.com');
+  this.password.sendKeys('password');
+  this.passRepeat.sendKeys('password');
+  }
+}
 module.exports = pageReg;
