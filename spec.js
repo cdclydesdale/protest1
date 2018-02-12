f3bc34d5739eac0110cd41c5441f3504405f9ee7
Clyde Dsouza <cdclydesdale@gmail.com>
2018-02-11 21:02:36 -0500

    3rd Commit

--- spec.js
+++ spec.js
@@ -1,5 +1,13 @@
+var helper = require('./helper.js');
 describe('it should open Etsy WebSite', function(){
-  browser.get(browser.params.url);
 
-  //expect(browser.getTitle()).toBe('');
+beforeEach(function(){
+  browser.get(browser.params.url);
+});
+
+it('should load the home page successfully',function(){
+  expect(browser.getTitle()).toBe('Etsy.com | Shop for anything from creative people everywhere');
+});
+
+  
 });
